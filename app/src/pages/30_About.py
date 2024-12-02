69353486839a0baae4d0f78cb0cc4207e6f1c278
carsondeas <148012025+carsondeas@users.noreply.github.com>
2024-12-02 15:29:06 -0500

    Added Student button to homepage and fixed About Page side links to allow going home.

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -1,18 +1,13 @@
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
-from modules.nav import SideBarLinks
+from modules.nav import HomeNav
 
-SideBarLinks()
+HomeNav()
 
-st.write("# About this App")
+st.write("# About SkillSeeker")
 
 st.markdown (
     """
-    This is a demo app for CS 3200 Course Project.  
-
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
-
-    Stay tuned for more information and features to come!
+    Write about our project here.
     """
         )
