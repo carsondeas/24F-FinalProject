90154a6dfb1cf5413266a51bc88403a9f28ea7f2
Ian Menachery <ianmenachery@gmail.com>
2024-12-02 17:57:08 -0500

    Update 30_About.py
    added some stuff to the about

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -8,6 +8,11 @@ st.write("# About SkillSeeker")
 
 st.markdown (
     """
-    Write about our project here.
-    """
+    ### **What is SkillSeeker?**
+    In today’s fast-evolving job market, **SkillSeeker** is a powerful, data-driven platform designed to bridge the information gap for in-demand skills. 
+    It helps students better prepare for their careers by informing them about the specific skills employers seek.
+
+    **SkillSeeker** simplifies the co-op and job application process by providing access to a comprehensive database of skills for potential employees and employers.
+    With this application, users can better understand the real demands of the industries they wish to enter, allowing for informed career planning and preparation.
+        """
         )
