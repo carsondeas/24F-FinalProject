af9fe632e1525d6916dd2e9a3e5c6728c3943756
jonathanwong05 <147997841+jonathanwong05@users.noreply.github.com>
2024-12-04 14:54:24 -0500

    Create progress tracking page and update student home page

--- app/src/pages/00_Student_Home.py
+++ app/src/pages/00_Student_Home.py
@@ -23,3 +23,8 @@ if st.button('Skill Gap Analysis',
              type='primary',
              use_container_width=True):
   st.switch_page('pages/02_skill_gap_analysis.py')
+
+if st.button("Progress Tracking",
+             type="primary",
+             use_container_width=True):
+  st.switch_page("pages/03_progress_tracking.py")
