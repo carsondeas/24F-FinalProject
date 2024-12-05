7892db827475828b674adecbd264aabbb48bf07b
jonathanwong05 <147997841+jonathanwong05@users.noreply.github.com>
2024-12-05 13:17:10 -0500

    fixed professor home page and skill trend page

--- app/src/pages/10_professor_home.py
+++ app/src/pages/10_professor_home.py
@@ -30,14 +30,14 @@ st.write("### What would you like to do today?")
 if st.button("Explore Skill Trends",
              type='primary',
              use_container_width=True):
-    st.switch_page('11_skill_trend.py')
+    st.switch_page('pages/11_skill_trend.py')
 
 if st.button("Compare Proficiency Levels",
              type='primary',
              use_container_width=True):
-    st.switch_page('12_compare_proficiency.py')
+    st.switch_page('pages/12_compare_proficiency.py')
 
 if st.button("View Top Skills for Co-ops",
              type='primary',
              use_container_width=True):
-    st.switch_page('13_top_skills.py')
+    st.switch_page('pages/13_top_skills.py')
