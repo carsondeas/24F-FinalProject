19966b9e18280e3020d46631f156bc4ba5bc3ede
tylerkhwang <tylerhwang0904@gmail.com>
2024-12-05 19:58:04 -0500

    reupload department route

--- app/src/pages/32_companyProfile.py
+++ app/src/pages/32_companyProfile.py
@@ -31,7 +31,7 @@ col1, col2, col3 = st.columns([1, 6, 1])
 with col1:
     if st.button("← Back"):
         st.write("Navigating back...")
-        st.switch_page('pages/4_cpAdvisorHome.py')
+        st.switch_page('pages/30_cpAdvisorHome.py')
 with col3:
     if st.button("🏠 Home"):
         st.write("Navigating to Home...") 
